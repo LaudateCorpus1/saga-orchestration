0b3a127a2ccaa9b830a5952325ee1ac395ee159a
prateekpandey25 <102592647+prateekpandey25@users.noreply.github.com>
2022-03-29 16:50:05 +0530

    Add files via upload

---
+++ event-driven-microservices-saga-orchestration/CommonService/src/main/java/com/studymaterial/CommonService/events/PaymentProcessedEvent.java
@@ -0,0 +1,13 @@
+package com.studymaterial.CommonService.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentProcessedEvent {
+    private String paymentId;
+    private String orderId;
+}
---
+++ event-driven-microservices-saga-orchestration/ProductService/src/main/java/com/studymaterial/ProductService/query/api/queries/GetProductsQuery.java
@@ -0,0 +1,4 @@
+package com.studymaterial.ProductService.query.api.queries;
+
+public class GetProductsQuery {
+}
